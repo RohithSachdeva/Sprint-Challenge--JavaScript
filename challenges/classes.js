ac43e2cfc3aee087130ce10abeb77e11144c67b2
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-02-28 09:37:42 -0800

    Need to finish classes and free response questions

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,12 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+function CuboidMaker (properties) {
+    this.length = properties.length;
+    this.width = properties.width;
+    this.height = properties.height;
+  };
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
